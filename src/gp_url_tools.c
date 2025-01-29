d6f296aeb1aa5b07af271cfa20ee15ff2935fb1a
Andrey Ovchinnikov <s21v_ovchinnikov@179.ru>
2025-01-29 11:53:49 +0300

    rework encode function: - shorter code - use sprintf for easier printing '%XX' characters - work with cstrings also add some support functions and rework get_hex function: - get_hex -> char2hex - clear way to process errors:

--- src/gp_url_tools.c
+++ src/gp_url_tools.c
@@ -16,82 +16,74 @@ Datum url_decode(PG_FUNCTION_ARGS);
 Datum uri_encode(PG_FUNCTION_ARGS);
 Datum uri_decode(PG_FUNCTION_ARGS);
 
-static const char *hex_chars = "0123456789ABCDEF";
+static bool allowed_character(const char c, const char *unreserved_special);
+static unsigned char char2hex(char c);
+static char *write_character(char *output, const char c);
+static text *encode(text *input, const char *unreserved_special);
 
-static const int8 hexlookup[128] = {
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,
-    9,  -1, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+static const unsigned char hexlookup[256] = {
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, -1, -1, -1,
+-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
 };
 
-static inline unsigned char get_hex(char c) {
-    int res = -1;
-
-    if (c > 0 && c < 127)
-        res = hexlookup[(unsigned char)c];
-
-    if (res < 0)
-        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-                        errmsg("invalid hexadecimal digit: \"%c\"", c)));
-
-    return (char)res;
+unsigned char char2hex(char c) {
+    unsigned char res = hexlookup[(unsigned char)c];
+    if (res < 0) {
+        ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("invalid hexadecimal digit: \"%c\"", c)));
+    }
+    return res;
 }
 
-static text *encode(text *in_text, const char *unreserved_special) {
-    int len;
-    text *result;
-    char *read_ptr;
-    char *write_ptr;
-    int real_len;
-    int processed;
-    int i;
+bool allowed_character(const char c, const char *unreserved_special) {
+    return ('0' <= c && c <= '9') || ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || (strchr(unreserved_special, c) != NULL);
+}
 
-    len = VARSIZE_ANY_EXHDR(in_text);
-    read_ptr = VARDATA_ANY(in_text);
+char *write_character(char *output, const char c) {
+    *output = c;
+    return ++output;
+}
 
-    /* preallocation max 3 times of size */
-    result = (text *)palloc(3 * len + VARHDRSZ);
-    write_ptr = VARDATA(result);
-    processed = 0;
-    real_len = 0;
-    while (processed < len) {
-        int mblen = pg_mblen(read_ptr);
+text *encode(text *input, const char *unreserved_special) {
+    int input_lenght;
+    text *output;
+    char *cinput, *coutput, *current;
 
-        if (mblen == 1) {
-            char c = *read_ptr;
+    // Convert input data for processing
+    cinput = text_to_cstring(input);
+    input_lenght = strlen(cinput);
+    // Allocate memory for result url string 
+    coutput = palloc(sizeof(*coutput) * (3 * input_lenght + 1));
+    current = coutput;
 
-            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||
-                (c >= 'A' && c <= 'Z') ||
-                (strchr(unreserved_special, c) != NULL)) {
-                *write_ptr++ = c;
-                real_len += 1;
-                processed += 1;
-                read_ptr += 1;
-
-                continue;
-            }
+    for (int i = 0; i < input_lenght; ++i) {
+        if (allowed_character(cinput[i], unreserved_special)) {
+            // single character => does not encode it or skip it
+            current = write_character(current, cinput[i]);
+        } else {
+            // some characters => process them all into '%XX' or '%XXXX' notation 
+            current += sprintf(current, "%%%02X", (unsigned char)cinput[i]);
         }
-
-        for (i = 0; i < mblen; i++) {
-            unsigned char b = ((unsigned char *)read_ptr)[i];
-
-            *write_ptr++ = '%';
-            *write_ptr++ = hex_chars[(b >> 4) & 0xF];
-            *write_ptr++ = hex_chars[b & 0xF];
-            real_len += 3;
-        }
-
-        processed += mblen;
-        read_ptr += mblen;
     }
+    current = write_character(current, 0);
 
-    SET_VARSIZE(result, real_len + VARHDRSZ);
-
-    return result;
+    // Convert to text and return
+    output = cstring_to_text(coutput);
+    pfree(coutput);
+    return output;
 }
 
 static uint32_t decode_utf16_pair(uint16_t c1, uint16_t c2) {
@@ -153,8 +145,8 @@ static text *decode(text *in_text, const char *unreserved_special) {
                 if (processed + 6 > len)
                     elog(ERROR, "incomplete input string");
 
-                b1 = (get_hex(read_ptr[2]) << 4) | get_hex(read_ptr[3]);
-                b2 = (get_hex(read_ptr[4]) << 4) | get_hex(read_ptr[5]);
+                b1 = (char2hex(read_ptr[2]) << 4) | char2hex(read_ptr[3]);
+                b2 = (char2hex(read_ptr[4]) << 4) | char2hex(read_ptr[5]);
 
                 /*
                  * expect input in UTF16-BE (Big Endian) and convert it
@@ -167,8 +159,8 @@ static text *decode(text *in_text, const char *unreserved_special) {
                     if (processed + 10 > len)
                         elog(ERROR, "incomplete input string");
 
-                    b1 = (get_hex(read_ptr[6]) << 4) | get_hex(read_ptr[7]);
-                    b2 = (get_hex(read_ptr[8]) << 4) | get_hex(read_ptr[9]);
+                    b1 = (char2hex(read_ptr[6]) << 4) | char2hex(read_ptr[7]);
+                    b2 = (char2hex(read_ptr[8]) << 4) | char2hex(read_ptr[9]);
                     c2 = b2 | (b1 << 8);
 
                     if (!(0xDC00 <= c2 && c2 <= 0xDFFF))
@@ -197,7 +189,7 @@ static text *decode(text *in_text, const char *unreserved_special) {
                     elog(ERROR, "incomplete input string");
 
                 *((unsigned char *)write_ptr++) =
-                    (get_hex(read_ptr[1]) << 4) | get_hex(read_ptr[2]);
+                    (char2hex(read_ptr[1]) << 4) | char2hex(read_ptr[2]);
                 real_len += 1;
                 processed += 3;
                 read_ptr += 3;
